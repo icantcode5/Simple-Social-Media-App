6f0632c8582b32b66d258d0de710e89b0d712097
Carlos Sosa <carlossosa54321@gmail.com>
2022-09-26 15:03:23 -0700

    added comments to BUB

---
+++ models/Comments.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  likes : {
+    type : Number,
+    required : true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
